f4fef5bdf193f1b5be0fa659a821eda07d4c08a2
lin-ister <60756387+lin-ister@users.noreply.github.com>
2021-01-29 23:33:21 -0800

    Add files via upload

---
+++ part1-question18.js
@@ -0,0 +1,6 @@
+function condition() {
+    for (var val in statistics) {
+        if (Object.key(statistics)[0] == 'r' && Object.values(statistics) == 2 % 1)
+            alert(Object.value(statistics);
+    }
+}
